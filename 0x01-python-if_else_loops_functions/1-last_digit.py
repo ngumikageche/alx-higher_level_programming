1ff4de6aaf3776b9b1d1d2b09a4e8b60c8b7a3c1
Joseph Kageche <ngumi98@gmail.com>
2023-03-07 17:24:08 +0300

    Positive anything is better than negative nothing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,4 @@ if l_digit > 5 and number > 0:
 elif l_digit == 0:
     print(f"Last digit of {number} is {l_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {l_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is -{l_digit} and is less than 6 and not 0")
