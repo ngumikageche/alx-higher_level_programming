bffb1c05bc73db84873a3889fc2ad68a179af39e
Joseph Kageche <ngumi98@gmail.com>
2023-04-01 13:36:17 +0300

    Safe list printing

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        positive = number * -1
-        l_digit = positive % 10
-        print("{}".format(l_digit), end="")
-        return (l_digit)
-    else:
-        l_digit = number % 10
-        print("{}".format(l_digit), end="")
-        return l_digit
+    # a function that prints the last digit of a number.
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
