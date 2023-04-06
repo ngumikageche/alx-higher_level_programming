5aa43349f074070c708bce219da8e29d0773f30a
Joseph Kageche <ngumi98@gmail.com>
2023-04-06 17:38:00 +0300

    fixing identation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
             width (int): the width of the Rectangle
             height (int): the height of the Rectangle
         """
-    self.width = width
-    self.height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
