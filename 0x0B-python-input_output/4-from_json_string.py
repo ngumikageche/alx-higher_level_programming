e6b0d3990ba25f6baf25aff8c28723121d9bb78a
Joseph Kageche <ngumi98@gmail.com>
2023-04-18 15:30:45 +0300

    Module documentation fixing

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python
 """Defines a func that conv From JSON string to Object"""
-
-
 import json
 
 
 def from_json_string(my_str):
-    """ func that returns an object represented by a json file
-    Args:
-        my_str (string): python string
-    Returns:
-        returns a python object
-    """
+    """ func that returns an object represented by a json file"""
     return json.loads(my_str)
