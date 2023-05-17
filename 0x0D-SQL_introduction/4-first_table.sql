2f8bfdacc852e62df20cc67e389cd3515dad14aa
Joseph Kageche <ngumi98@gmail.com>
2023-05-17 17:07:25 +0300

    fix

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,4 @@
-/*! Creates a table first_table
-    args: 
-        id (int) - 
-	name(VARCHAR(256))
-*/
+-- Creates a table first_table args: id (int) - name(VARCHAR(256))
 CREATE TABLE IF NOT EXISTS 'first_table' (
        'id' int;
        'name' varchar(256)
