a99ad3b090e6e5e44b1ab6e3b6f3ad2591afe68c
Joseph Kageche <ngumi98@gmail.com>
2023-06-02 10:56:48 +0300

    Documenting

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,19 @@ from models.base import Base
 class Rectangle(Base):
     """body of rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new rectangle
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+            x (int):The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
         super().__init__(id)
         self.__width = width
         self.__height = height
